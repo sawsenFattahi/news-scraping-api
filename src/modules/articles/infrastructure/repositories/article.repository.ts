24ef2a5e70c5dac746433db0535253e290b04bc3
sawsenFattahi <sawsen.fattahi@gmail.com>
2025-02-21 19:07:10 +0100

    use custom log library

--- src/modules/articles/infrastructure/repositories/article.repository.ts
+++ src/modules/articles/infrastructure/repositories/article.repository.ts
@@ -1,7 +1,8 @@
 import { BadRequestException, Inject, Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { Cache } from 'cache-manager';
+import { logAsync } from 'perf-async-logger';
+import { Repository } from 'typeorm';
 
 import { ICreateArticle } from '@ns/modules/articles/applications/interfaces';
 import { ArticleRepository } from '@ns/modules/articles/domain/repository-adapters';
@@ -12,9 +13,11 @@ export default class ArticleRepositoryImpl implements ArticleRepository {
   constructor(
     @InjectRepository(Article)
     private readonly repo: Repository<Article>,
-    @Inject('CACHE_MANAGER') private cacheManager: Cache
+    @Inject('CACHE_MANAGER') private cacheManager: Cache,
   ) {}
   private readonly logger = new Logger(ArticleRepositoryImpl.name);
+
+  @logAsync
   async create(createArticle: ICreateArticle): Promise<Article> {
     try {
       this.logger.log('Start saving article');
@@ -30,46 +33,41 @@ export default class ArticleRepositoryImpl implements ArticleRepository {
     }
   }
 
+  @logAsync
   async findAll(limit: number, page: number): Promise<Article[]> {
     try {
-      // ✅ Test Redis
       await this.cacheManager.set('test-key', 'Hello Redis', 300);
       const value = await this.cacheManager.get('test-key');
-      console.log(value);  // ✅ Devrait afficher "Hello Redis"
-  
+
       this.logger.log('Start finding articles');
       const cacheKey = `articles:${page}:${limit}`;
       this.logger.log(`Cache key: ${cacheKey}`);
-  
-      // ✅ Récupérer la valeur brute
+
       const cached = await this.cacheManager.get<string>(cacheKey);
       this.logger.log(`Cached value: ${cached}`);
-  
-      // ✅ Désérialiser la chaîne JSON
+
       if (cached) {
-        const articles = JSON.parse(cached); // Convertir la chaîne JSON en tableau d'articles
+        const articles = JSON.parse(cached);
         this.logger.log('Found cached articles');
+
         return articles;
       }
-  
-      // ✅ Requête SQL si le cache est vide
+
       const articles = await this.repo.find({
         take: limit,
         skip: (page - 1) * limit,
         order: { publishedAt: 'DESC' },
       });
-  
+
       this.logger.log(`Found ${articles.length} articles`);
-  
-      // ✅ Sérialisation lors de la mise en cache
+
       await this.cacheManager.set(cacheKey, JSON.stringify(articles), 800);
       this.logger.log('Articles saved in cache', cacheKey);
-  
+
       return articles;
     } catch (error) {
       this.logger.error(`Error finding articles: ${error.message}`);
       throw new BadRequestException(`Error finding articles: ${error.message}`);
     }
   }
-  
 }
